d845071d1fa3a2afa9affafc46c8249783514d46
7vikkumar15 <7vik.kumar15@gmail.com>
2026-02-12 12:12:03 +0530

    Add files via upload

---
+++ class3project.py
@@ -0,0 +1,16 @@
+# String operations example – Congratulations message
+
+name = "Saatvik"
+achievement = "passed the exam"
+
+# joining strings
+message = "Congratulations " + name + " for " + achievement + "!"
+
+print(message)
+
+# converting to uppercase
+print(message.upper())
+
+# repeating the message
+print((message + " ") * 2)
+
